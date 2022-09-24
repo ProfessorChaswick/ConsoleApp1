a1c65e5591eb613f5f18eca1547a2c4222cdde56
Charles Hildebrant <ceh2624@gmail.com>
2022-09-24 17:35:06 -0400

    Add project files.

---
+++ ConsoleApp1/Program.vb
@@ -0,0 +1,9 @@
+Imports System
+Imports RollDice.Roll REM to call the rollEm method from the C# class in the RollDice.dll file.
+Module Program
+    Sub Main(args As String())
+        Dim num As Integer
+        num = rollEm()
+        Console.WriteLine("Hello World! The number is " & num)
+    End Sub
+End Module
